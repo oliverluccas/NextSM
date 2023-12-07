f63f3aeed3daafe6f2540c881267e571e40ff79e
oliverluccas <153122650+oliverluccas@users.noreply.github.com>
2023-12-07 12:07:18 -0300

    Update trello-api.js

--- trello-api.js
+++ trello-api.js
@@ -1,14 +1,11 @@
-const trello = window.TrelloPowerUp.iframe();
+import { iframe } from 'trello-power-up';
 
 const getListsOnBoard = () => {
-  return trello.board('all').then(board => board.lists);
+  return iframe().board('all').then(board => board.lists);
 };
 
 const getCardsOnList = (listId) => {
-  return trello.cards('all').then(cards => cards.filter(card => card.idList === listId));
+  return iframe().cards('all').then(cards => cards.filter(card => card.idList === listId));
 };
 
-module.exports = {
-  getListsOnBoard,
-  getCardsOnList
-};
+export { getListsOnBoard, getCardsOnList };
